1f0d642ba0f79befac22570e573d631092adf24e
EduMeurer999 <Eduardo8890>
2018-08-23 10:10:56 -0300

    Python time e zip

---
+++ venv/exercicios_Time.py
@@ -0,0 +1,8 @@
+import time
+fim = "21/12/2018"
+agora=time.strptime("21 Dec 18", "%d %b %y")
+
+print(agora)
+
+
+#tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec
---
+++ venv/exercícioZIP.py
@@ -0,0 +1,10 @@
+import os,zipfile
+os.chdir(os.getcwd()+"\\AulaPython\\Arquivos\\Documentos")
+zip = zipfile.ZipFile('varios.zip')
+print(zip.namelist())
+fileInfo = zip.getinfo('Novo(a) Texto OpenDocument.odt')
+print(fileInfo.file_size/1024)
+newZip = zipfile.ZipFile('variosPdf.zip', 'w')
+newZip.write('Aula 12 Diretorios Arquivos.pdf', 'Aula 13 Zip Data e Hora.pdf')
+newZip.close()
+zip.extract('python.odt')
